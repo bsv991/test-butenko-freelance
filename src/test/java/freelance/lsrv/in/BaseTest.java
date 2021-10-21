53d5540817371a2184297d3eb1fc7ecf22bb05df
Sergey Butenko <sergej.butenko@privatbank.ua>
2021-10-21 15:17:31 +0300

    Добавлен дефолтный путь для отчетов

--- src/test/java/freelance/lsrv/in/BaseTest.java
+++ src/test/java/freelance/lsrv/in/BaseTest.java
@@ -1,16 +1,8 @@
 package freelance.lsrv.in;
 
 import com.codeborne.selenide.Configuration;
-import io.qameta.allure.Attachment;
-import lombok.SneakyThrows;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import java.io.File;
-import java.nio.file.Files;
+
 import java.nio.file.Paths;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,6 +12,7 @@ public class BaseTest {
         Configuration.browser = "chrome";
         Configuration.baseUrl = "https://freelance.lsrv.in.ua/";
         Configuration.downloadsFolder = Paths.get("target").toFile().getAbsolutePath();
+        Configuration.reportsFolder = Paths.get("target").toFile().getAbsolutePath();
         Configuration.startMaximized = true;
     }
 
